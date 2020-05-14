889374239d6fc1fea8d61ba08a6fbce213f648b8
Kesa Shabarish <shabarish.shabbi@gmail.com>
2020-05-14 02:11:05 -0400

    update
    DELETE Added
    Added Interceptors of axios - used to add headers
    added Axios Instance

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+
+axios.interceptors.request.use(requestConfig => {
+    console.log(requestConfig);
+    return requestConfig;
+});
+
+axios.interceptors.response.use(response => {
+    // console.log(response);
+    return response;
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
